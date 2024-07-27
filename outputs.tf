c4ac26eff65c13acb41faff4aae24501a2dd16f1
wineperm <leo_kraska@mail.ru>
2024-07-27 15:43:22 +0500

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "master_ips" {
+  value = [for master in yandex_compute_instance.k8s-master : master.network_interface.0.nat_ip_address]
+}
+
+output "worker_ips" {
+  value = [for worker in yandex_compute_instance.k8s-worker : worker.network_interface.0.nat_ip_address]
+}
