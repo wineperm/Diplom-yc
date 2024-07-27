8ea440d34353a875a72e1106d30158a3793b6775
wineperm <leo_kraska@mail.ru>
2024-07-27 14:38:32 +0500

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -29,13 +29,13 @@ resource "yandex_storage_bucket" "test" {
   }
 }
 
-// Запись объекта в бакет
-resource "yandex_storage_object" "netology_image" {
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket     = yandex_storage_bucket.test.bucket
-  key        = "KLS_netology_12.07.2004.jpeg"
-  source     = "./KLS_netology_12.07.2004.jpeg"
-  acl        = "public-read"
-  content_type = "image/jpeg"
-}
+#// Запись объекта в бакет
+#resource "yandex_storage_object" "netology_image" {
+#  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#  bucket     = yandex_storage_bucket.test.bucket
+#  key        = "KLS_netology_12.07.2004.jpeg"
+#  source     = "./KLS_netology_12.07.2004.jpeg"
+#  acl        = "public-read"
+#  content_type = "image/jpeg"
+#}
