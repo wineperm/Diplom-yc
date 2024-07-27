83e6fd44363039213634bf68d0fbebabe81cdee6
wineperm <leo_kraska@mail.ru>
2024-07-27 20:17:14 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">=0.13"
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#  required_version = ">=0.13"
+#}
 
 provider "yandex" {
   cloud_id  = var.yc_cloud_id
