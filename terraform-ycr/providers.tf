e45f9b7ab5c910aaa139047abc5ee71e4f2c2d12
wineperm <leo_kraska@mail.ru>
2024-07-27 16:42:12 +0500

    Create providers.tf

---
+++ terraform-ycr/providers.tf
@@ -0,0 +1,12 @@
+variable "yc_cloud_id" {
+  description = "Идентификатор облака Yandex"
+}
+
+variable "yc_folder_id" {
+  description = "Идентификатор папки Yandex"
+}
+
+variable "yc_zone" {
+  description = "Зона Yandex Cloud"
+  default     = "ru-central1-a"
+}
