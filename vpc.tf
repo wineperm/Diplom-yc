6b00d8c6da474a4991ba5a10e6c62557ded83337
wineperm <leo_kraska@mail.ru>
2024-07-27 11:15:01 +0500

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+resource "yandex_vpc_network" "default" {
+  name = "default-network"
+}
+
+resource "yandex_vpc_subnet" "subnet-a" {
+  name           = "subnet-a"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.default.id
+  v4_cidr_blocks = ["10.0.1.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet-b" {
+  name           = "subnet-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.default.id
+  v4_cidr_blocks = ["10.0.2.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet-d" {
+  name           = "subnet-d"
+  zone           = "ru-central1-d"
+  network_id     = yandex_vpc_network.default.id
+  v4_cidr_blocks = ["10.0.3.0/24"]
+}
