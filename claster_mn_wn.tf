faf5847f31a6f1c8e865a96178af568bf11e6d99
wineperm <leo_kraska@mail.ru>
2024-07-27 13:28:33 +0500

    Create claster_mn_wn.tf

---
+++ claster_mn_wn.tf
@@ -0,0 +1,53 @@
+// Создание виртуальных машин для мастер-узлов
+resource "yandex_compute_instance" "k8s-master" {
+  count       = 3
+  name        = "k8s-master-${count.index}"
+  platform_id = "standard-v2"
+  zone        = element(["ru-central1-a", "ru-central1-b", "ru-central1-d"], count.index)
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1"
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+  network_interface {
+    subnet_id = element([yandex_vpc_subnet.master-subnet-a.id, yandex_vpc_subnet.master-subnet-b.id, yandex_vpc_subnet.master-subnet-d.id], count.index)
+    nat       = true
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+}
+
+// Создание виртуальных машин для воркер-узлов
+resource "yandex_compute_instance" "k8s-worker" {
+  count       = 6
+  name        = "k8s-worker-${count.index}"
+  platform_id = "standard-v2"
+  zone        = element(["ru-central1-a", "ru-central1-b", "ru-central1-d"], count.index % 3)
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1"
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+  network_interface {
+    subnet_id = element([yandex_vpc_subnet.worker-subnet-a.id, yandex_vpc_subnet.worker-subnet-b.id, yandex_vpc_subnet.worker-subnet-d.id], count.index % 3)
+    nat       = true
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+}
