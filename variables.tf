9d68ebe7fa3b7de5746b9fdce47b7eb74c2398b9
wineperm <leo_kraska@mail.ru>
2024-07-28 15:08:07 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,6 +18,11 @@ variable "ssh_public_key" {
   type        = string
 }
 
+variable "ssh_private_key_path" {
+  description = "Path to the SSH private key file"
+  type        = string
+}
+
 variable "master_vm_count" {
   description = "Количество мастер-узлов"
   type        = number
