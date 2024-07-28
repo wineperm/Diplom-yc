29838a71d16e9a6a95659c18ac0f7e93c9733216
wineperm <leo_kraska@mail.ru>
2024-07-28 10:41:52 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,7 +12,7 @@ provider "yandex" {
   cloud_id  = var.yc_cloud_id
   folder_id = var.yc_folder_id
   zone      = var.yc_zone
-  service_account_key_file = file("~/.ssh/authorized_key.json")
+#  service_account_key_file = file("~/.ssh/authorized_key.json")
 }
 
 provider "local" {}
