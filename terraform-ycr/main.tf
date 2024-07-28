63f75a783c2f0c1be5fedc861abd302ebb0357df
wineperm <leo_kraska@mail.ru>
2024-07-28 11:08:38 +0500

    Delete terraform-ycr directory

--- terraform-ycr/main.tf
+++
@@ -1,4 +0,0 @@
-resource "yandex_container_registry" "my_registry" {
-  name = "my-registry"
-  folder_id = var.yc_folder_id
-}
--- terraform-ycr/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "registry_id" {
-  value = yandex_container_registry.my_registry.id
-}
-
-output "registry_name" {
-  value = yandex_container_registry.my_registry.name
-}
