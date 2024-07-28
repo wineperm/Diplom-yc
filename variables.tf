e8aa17a6ba4cefc532b446ab2cb2eb6d8088b193
wineperm <leo_kraska@mail.ru>
2024-07-28 15:10:41 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,42 +1,21 @@
 variable "yc_cloud_id" {
   description = "Идентификатор облака Yandex"
-  type        = string
 }
 
 variable "yc_folder_id" {
   description = "Идентификатор папки Yandex"
-  type        = string
+}
+
+variable "yc_zone" {
+  description = "Зона Yandex Cloud"
+  default     = "ru-central1-a"
 }
 
 variable "yc_service_account_id" {
   description = "Идентификатор сервисного аккаунта"
-  type        = string
 }
 
 variable "ssh_public_key" {
   description = "SSH public key for accessing the instances"
   type        = string
-}
-
-variable "ssh_private_key_path" {
-  description = "Path to the SSH private key file"
-  type        = string
-}
-
-variable "master_vm_count" {
-  description = "Количество мастер-узлов"
-  type        = number
-  default     = 1
-}
-
-variable "worker_vm_count" {
-  description = "Количество воркер-узлов"
-  type        = number
-  default     = 1
-}
-
-variable "zones" {
-  description = "Список зон для создания подсетей"
-  type        = list(string)
-  default     = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
 }
