a96bf5d68f1ee95aff7d87bb600443c565fc1fcf
wineperm <leo_kraska@mail.ru>
2024-07-28 15:44:01 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket"
-    key    = "terraform.tfstate"
-    region = "ru-central1"
-    access_key = var.yc_access_key
-    secret_key = var.yc_secret_key
-    endpoint = "storage.yandexcloud.net"
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "terraform-state-bucket"
+    region     = "ru-central1"
+    key        = "terraform.tfstate"
+    access_key = "${YC_ACCESS_KEY}"
+    secret_key = "${YC_SECRET_KEY}"
   }
 }
