25ceac243a94132546b8c1f21cab75e458882848
wineperm <leo_kraska@mail.ru>
2024-07-28 12:33:30 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  token     = var.yc_token
+#  token     = var.yc_token
   cloud_id  = var.yc_cloud_id
   folder_id = var.yc_folder_id
   zone      = var.yc_zone
