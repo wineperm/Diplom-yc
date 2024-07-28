9866adcfb10581fcff3e0e3e17c95144d9467c11
wineperm <leo_kraska@mail.ru>
2024-07-28 14:52:40 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,14 +29,14 @@ resource "yandex_compute_instance" "k8s-master" {
   service_account_id = var.yc_service_account_id
 
   provisioner "file" {
-    source      = "~/.ssh/id_ed25519"
+    source      = "${path.module}/.ssh/id_ed25519"
     destination = "/home/ubuntu/.ssh/id_ed25519"
 
     connection {
       type        = "ssh"
       user        = "ubuntu"
       host        = self.network_interface.0.nat_ip_address
-      private_key = file("~/.ssh/id_ed25519")
+      private_key = file("${path.module}/.ssh/id_ed25519")
     }
   }
 }
@@ -72,15 +72,14 @@ resource "yandex_compute_instance" "k8s-worker" {
   service_account_id = var.yc_service_account_id
 
   provisioner "file" {
-    source      = "~/.ssh/id_ed25519"
+    source      = "${path.module}/.ssh/id_ed25519"
     destination = "/home/ubuntu/.ssh/id_ed25519"
 
     connection {
       type        = "ssh"
       user        = "ubuntu"
       host        = self.network_interface.0.nat_ip_address
-      private_key = file("~/.ssh/id_ed25519")
+      private_key = file("${path.module}/.ssh/id_ed25519")
     }
   }
 }
-
