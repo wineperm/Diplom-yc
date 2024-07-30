4248ee2034588479ec935f51a6fc2e790ce269a5
wineperm <leo_kraska@mail.ru>
2024-07-30 22:26:03 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,10 +5,21 @@ terraform {
     }
   }
   required_version = ">=0.13"
+
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "wineperm12354464"
+    region   = "ru-central1"
+    key      = "terraform.tfstate"
+    access_key = var.yc_access_key
+    secret_key = var.yc_secret_key
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 provider "yandex" {
-#  token     = var.yc_token
   cloud_id  = var.yc_cloud_id
   folder_id = var.yc_folder_id
   zone      = var.yc_zone
