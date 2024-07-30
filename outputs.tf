cb61b898fca8fb5e3a8d1912abe357610051934b
wineperm <leo_kraska@mail.ru>
2024-07-30 20:16:13 +0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "master_ips" {
 output "worker_ips" {
   value = yandex_compute_instance.k8s-worker[*].network_interface.0.nat_ip_address
 }
+
+output "master_internal_ips" {
+  value = yandex_compute_instance.k8s-master[*].network_interface.0.ip_address
+}
+
+output "worker_internal_ips" {
+  value = yandex_compute_instance.k8s-worker[*].network_interface.0.ip_address
+}
