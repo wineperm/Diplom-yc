76f87e8006d830e3af8bdd3c6ac6bd31dc6e1697
wineperm <leo_kraska@mail.ru>
2024-07-30 22:58:56 +0500

    Update backet.tf

--- backet.tf
+++ backet.tf
@@ -28,3 +28,15 @@ resource "yandex_storage_bucket" "test" {
     enabled = true
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket"
+    key    = "terraform.tfstate"
+    region = "ru-central1"
+    access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+    secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+    endpoint = "storage.yandexcloud.net"
+  }
+}
+
