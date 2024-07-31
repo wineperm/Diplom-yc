dd3388d1f012846aca32f0453bd92ea910e241d4
wineperm <leo_kraska@mail.ru>
2024-07-31 18:03:53 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,13 +59,13 @@ resource "yandex_compute_instance" "k8s-worker" {
 }
 
 
-# resource "null_resource" "check_ssh_connection" {
-#   depends_on = [yandex_compute_instance.k8s-master, yandex_compute_instance.k8s-worker]
+resource "null_resource" "check_ssh_connection" {
+  depends_on = [yandex_compute_instance.k8s-master, yandex_compute_instance.k8s-worker]
 
-#   provisioner "local-exec" {
-#     command = "until ssh -o ConnectTimeout=5 -i ${var.ssh_private_key_path} ubuntu@${yandex_compute_instance.k8s-master[0].network_interface.0.nat_ip_address} echo 'SSH connection successful'; do sleep 5; done"
-#   }
-# }
+  provisioner "local-exec" {
+    command = "until ssh -o ConnectTimeout=5 -i ${var.ssh_private_key_path} ubuntu@${yandex_compute_instance.k8s-master[0].network_interface.0.nat_ip_address} echo 'SSH connection successful'; do sleep 5; done"
+  }
+}
 
 # resource "null_resource" "run_additional_commands" {
 #   depends_on = [null_resource.check_ssh_connection]
