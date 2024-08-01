95098409abe563d0d169ceddcaa5b1f620b046c4
wineperm <leo_kraska@mail.ru>
2024-08-01 18:37:30 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,6 +59,11 @@ resource "yandex_compute_instance" "k8s-worker" {
 }
 
 resource "null_resource" "check_ssh_connection" {
+  depends_on = [
+    yandex_compute_instance.k8s-master,
+    yandex_compute_instance.k8s-worker
+  ]
+
   provisioner "local-exec" {
     command = <<EOT
       #!/bin/sh
