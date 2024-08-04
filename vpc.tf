817fd50bc6bfda0e64a12bc88cbe86a09f63f962
wineperm <leo_kraska@mail.ru>
2024-08-04 18:33:41 +0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,57 +1,24 @@
-# Создание сети для мастер-узлов
-resource "yandex_vpc_network" "master-network" {
-  name = "master-network"
+resource "yandex_vpc_network" "k8s-network" {
+  name = "k8s-network"
 }
 
-# Создание подсети для мастер-узлов в зоне ru-central1-a
-resource "yandex_vpc_subnet" "master-subnet-a" {
-  name           = "master-subnet-a"
+resource "yandex_vpc_subnet" "k8s-subnet-a" {
+  name           = "k8s-subnet-a"
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.master-network.id
+  network_id     = yandex_vpc_network.k8s-network.id
   v4_cidr_blocks = ["10.1.1.0/24"]
 }
 
-# Создание подсети для мастер-узлов в зоне ru-central1-b
-resource "yandex_vpc_subnet" "master-subnet-b" {
-  name           = "master-subnet-b"
+resource "yandex_vpc_subnet" "k8s-subnet-b" {
+  name           = "k8s-subnet-b"
   zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.master-network.id
+  network_id     = yandex_vpc_network.k8s-network.id
   v4_cidr_blocks = ["10.1.2.0/24"]
 }
 
-# Создание подсети для мастер-узлов в зоне ru-central1-d
-resource "yandex_vpc_subnet" "master-subnet-d" {
-  name           = "master-subnet-d"
+resource "yandex_vpc_subnet" "k8s-subnet-d" {
+  name           = "k8s-subnet-d"
   zone           = "ru-central1-d"
-  network_id     = yandex_vpc_network.master-network.id
+  network_id     = yandex_vpc_network.k8s-network.id
   v4_cidr_blocks = ["10.1.3.0/24"]
-}
-
-# Создание сети для воркер-узлов
-resource "yandex_vpc_network" "worker-network" {
-  name = "worker-network"
-}
-
-# Создание подсети для воркер-узлов в зоне ru-central1-a
-resource "yandex_vpc_subnet" "worker-subnet-a" {
-  name           = "worker-subnet-a"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.worker-network.id
-  v4_cidr_blocks = ["10.2.1.0/24"]
-}
-
-# Создание подсети для воркер-узлов в зоне ru-central1-b
-resource "yandex_vpc_subnet" "worker-subnet-b" {
-  name           = "worker-subnet-b"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.worker-network.id
-  v4_cidr_blocks = ["10.2.2.0/24"]
-}
-
-# Создание подсети для воркер-узлов в зоне ru-central1-d
-resource "yandex_vpc_subnet" "worker-subnet-d" {
-  name           = "worker-subnet-d"
-  zone           = "ru-central1-d"
-  network_id     = yandex_vpc_network.worker-network.id
-  v4_cidr_blocks = ["10.2.3.0/24"]
 }
