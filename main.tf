de49979933d9caf3834c537b57c393188961e218
wineperm <leo_kraska@mail.ru>
2024-08-04 18:40:15 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "yandex_compute_instance" "k8s-master" {
     }
   }
   network_interface {
-    subnet_id = element([yandex_vpc_subnet.master-subnet-a.id, yandex_vpc_subnet.master-subnet-b.id, yandex_vpc_subnet.master-subnet-d.id], count.index)
+    subnet_id = element([yandex_vpc_subnet.k8s-subnet-a.id, yandex_vpc_subnet.k8s-subnet-b.id, yandex_vpc_subnet.k8s-subnet-d.id], count.index)
     nat       = true
   }
   scheduling_policy {
@@ -27,18 +27,6 @@ resource "yandex_compute_instance" "k8s-master" {
   }
   service_account_id = var.yc_service_account_id
 }
-#  provisioner "remote-exec" {
-#    inline = [
-#      "echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo"
-#    ]
-#    connection {
-#      type        = "ssh"
-#      user        = "ubuntu"
-#      private_key = file(var.ssh_private_key_path)
-#      host        = self.network_interface.0.nat_ip_address
-#    }
-#  }
-#}
 
 resource "yandex_compute_instance" "k8s-worker" {
   count       = 2
@@ -58,8 +46,8 @@ resource "yandex_compute_instance" "k8s-worker" {
     }
   }
   network_interface {
-    subnet_id = element([yandex_vpc_subnet.worker-subnet-a.id, yandex_vpc_subnet.worker-subnet-b.id, yandex_vpc_subnet.worker-subnet-d.id], count.index % 3)
-    nat       = false  # Установите nat в false для рабочих узлов
+    subnet_id = element([yandex_vpc_subnet.k8s-subnet-a.id, yandex_vpc_subnet.k8s-subnet-b.id, yandex_vpc_subnet.k8s-subnet-d.id], count.index % 3)
+    nat       = false
   }
   scheduling_policy {
     preemptible = true
@@ -69,56 +57,3 @@ resource "yandex_compute_instance" "k8s-worker" {
   }
   service_account_id = var.yc_service_account_id
 }
-
-#  provisioner "remote-exec" {
-#    inline = [
-#      "echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo"
-#    ]
-#    connection {
-#      type        = "ssh"
-#      user        = "ubuntu"
-#      private_key = file(var.ssh_private_key_path)
-#      host        = self.network_interface.0.nat_ip_address
-#    }
-#  }
-#}
-
-
-
-
-
-#resource "null_resource" "wait_for_outputs" {
-#  depends_on = [
-#    yandex_compute_instance.k8s-master,
-#    yandex_compute_instance.k8s-worker
-#  ]
-#
-#  provisioner "local-exec" {
-#    command = "echo 'Waiting for outputs to be ready...'"
-#  }
-#}
-
-#resource "null_resource" "check_ssh_connection" {
-#  depends_on = [
-#    null_resource.wait_for_outputs
-#  ]
-
-#  provisioner "local-exec" {
-#    command = <<EOT
-#      #!/bin/sh
-#      MASTER_IPS=$(terraform output -json master_external_ips | jq -r '.[]')
-#      if [ -z "$MASTER_IPS" ]; then
-#        echo "Не найдено внешних IP-адресов мастеров."
-#        exit 1
-#      fi
-#      for host in $MASTER_IPS; do
-#        while ! nc -zv $host 22; do
-#          echo "Ожидание SSH-соединения с $host..."
-#          sleep 10
-#        done
-#        echo "SSH-соединение с $host установлено"
-#      done
-#    EOT
-#  }
-#}
-
