2ff027184bc9461041f15f6224bc0ba3e718b05c
wineperm <leo_kraska@mail.ru>
2024-08-05 10:25:55 +0500

    Create runner.tf

---
+++ runner.tf
@@ -0,0 +1,28 @@
+resource "yandex_compute_instance" "runner" {
+  name        = "runner"
+  platform_id = "standard-v2"
+  zone        = "ru-central1-a"
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8k2vlv3b3duv812ama"
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.k8s-subnet-a.id
+    nat       = true
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${var.ssh_public_key}"
+  }
+  service_account_id = var.yc_service_account_id
+}
