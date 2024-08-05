258d86e76292b5d7551e9bbb9f020573afffc8c5
wineperm <leo_kraska@mail.ru>
2024-08-05 15:40:02 +0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-output "master_external_ips" {
-  value = yandex_compute_instance.k8s-master[*].network_interface.0.nat_ip_address
-}
-
-output "master_internal_ips" {
-  value = yandex_compute_instance.k8s-master[*].network_interface.0.ip_address
-}
-
-output "worker_internal_ips" {
-  value = yandex_compute_instance.k8s-worker[*].network_interface.0.ip_address
-}
-
 output "runner_external_ip" {
   value = yandex_compute_instance.runner.network_interface.0.nat_ip_address
 }
@@ -18,6 +6,19 @@ output "master_names" {
   value = yandex_compute_instance.k8s-master[*].name
 }
 
+output "master_external_ips" {
+  value = yandex_compute_instance.k8s-master[*].network_interface.0.nat_ip_address
+}
+
+output "master_internal_ips" {
+  value = yandex_compute_instance.k8s-master[*].network_interface.0.ip_address
+}
+
 output "worker_names" {
   value = yandex_compute_instance.k8s-worker[*].name
 }
+
+output "worker_internal_ips" {
+  value = yandex_compute_instance.k8s-worker[*].network_interface.0.ip_address
+}
+
