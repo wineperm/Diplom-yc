30201d3bd94d53e0046691e88a41d7e15398eb7e
wineperm <leo_kraska@mail.ru>
2024-08-06 12:40:16 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "yandex_compute_instance" "k8s-worker" {
   }
   network_interface {
     subnet_id = element([yandex_vpc_subnet.k8s-subnet-a.id, yandex_vpc_subnet.k8s-subnet-b.id, yandex_vpc_subnet.k8s-subnet-d.id], count.index % 3)
-    nat       = true
+    nat       = false # false # true
   }
   scheduling_policy {
     preemptible = true
