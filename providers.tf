130efc00c45b6c4f3f8d85cf127f06d87cfc0385
wineperm <leo_kraska@mail.ru>
2024-08-07 10:12:10 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,24 +6,6 @@ terraform {
   }
   required_version = ">= 0.13"
 
-  backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
-    bucket = "wineperm-tfstate-bucket"
-    region = "ru-central1"
-    key    = "terraform.tfstate"
-
-    access_key = aws_access_key_id
-    secret_key = aws_secret_access_key
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-  }
-}
-
 provider "yandex" {
   cloud_id                 = var.yc_cloud_id
   folder_id                = var.yc_folder_id
