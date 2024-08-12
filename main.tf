00e05d9d7d2dda8e39d6e4637a1a995a3b6380f6
wineperm <leo_kraska@mail.ru>
2024-08-12 10:25:42 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ resource "yandex_compute_instance" "k8s-master" {
   }
   network_interface {
     subnet_id = element([yandex_vpc_subnet.k8s-subnet-a.id, yandex_vpc_subnet.k8s-subnet-b.id, yandex_vpc_subnet.k8s-subnet-d.id], count.index)
+    ip_address = cidrhost(element(["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"], count.index), count.index + 10)
     nat       = true # false # true
   }
   scheduling_policy {
@@ -47,7 +48,8 @@ resource "yandex_compute_instance" "k8s-worker" {
   }
   network_interface {
     subnet_id = element([yandex_vpc_subnet.k8s-subnet-a.id, yandex_vpc_subnet.k8s-subnet-b.id, yandex_vpc_subnet.k8s-subnet-d.id], count.index % 3)
-    nat       = true # false # true
+    ip_address = cidrhost(element(["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"], count.index % 3), count.index + 20)
+    nat       = false # false # true
   }
   scheduling_policy {
     preemptible = true
