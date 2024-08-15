fd9c387b9533ec9f561588df88621de9c83399c4
wineperm <leo_kraska@mail.ru>
2024-08-15 16:02:21 +0500

    Update update_terraform_config.sh

--- update_terraform_config.sh
+++ update_terraform_config.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Изменяем конфигурацию Terraform для закрытия внешней сети у воркеров
-sed -i '/resource "yandex_compute_instance" "k8s-worker"/,/}/ {
-  /nat       = true/ {
-    s/nat       = true/nat       = false/
-  }
-}' terraform/main.tf
+# # Изменяем конфигурацию Terraform для закрытия внешней сети у воркеров
+# sed -i '/resource "yandex_compute_instance" "k8s-worker"/,/}/ {
+#   /nat       = true/ {
+#     s/nat       = true/nat       = false/
+#   }
+# }' terraform/main.tf
 
-# Применяем изменения
-terraform apply -auto-approve
+# # Применяем изменения
+# terraform apply -auto-approve
